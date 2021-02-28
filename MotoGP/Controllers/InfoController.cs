38d0efcb5522759093ebcbc7daf1a1b15b71b1cd
Stijn Roelants <stijn.roelants@gmail.com>
2021-02-28 11:20:18 +0100

    Einde_deel1

---
+++ MotoGP/Controllers/InfoController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MotoGP.Models;
+
+namespace MotoGP.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult ListRaces()
+        {
+            int banner = 1;
+            ViewData["Banner"] = banner;
+            return View();
+        }
+
+        public IActionResult ListTeams()
+        {
+            int banner = 2;
+            ViewData["Banner"] = banner;
+            return View();
+        }
+
+        public IActionResult ListRiders()
+        {
+            int banner = 3;
+            ViewData["Banner"] = banner;
+            return View();
+        }
+
+        public IActionResult BuildMap()
+        {
+            List<Race> races = new List<Race>();
+            races.Add(new Race() { RaceID = 1, X = 517, Y = 19, Name = "Assen" });
+            races.Add(new Race() { RaceID = 2, X = 859, Y = 249, Name = "Losail Circuit" });
+            races.Add(new Race() { RaceID = 3, X = 194, Y = 428, Name = "Autódromo Termas de Río Hondo" });
+
+            ViewData["Banner"] = 1;
+            return View(races);
+        }
+    }
+}
---
+++ MotoGP/Controllers/ShopController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotoGP.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult OrderTicket()
+        {
+            ViewData["Banner"] = 4;
+            return View();
+        }
+
+        public IActionResult ConfirmOrder()
+        {
+            DateTime dateTime = DateTime.Today;
+            ViewData["Banner"] = 4;
+            ViewData["Message"] = $"Thank you for ordering tickets! Your booking is registered on {dateTime.ToString("dd/MM/yyyy")}. We will send out your tickets in the coming week.";
+            return View();
+        }
+    }
+}
